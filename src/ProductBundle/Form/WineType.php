b5a160352a9d353ebd0f21ee1a6adc6f4f6a0cf7
rgrisot <grisot.remi@gmail.com>
2017-01-11 09:46:30 +0100

    Generate CRUD Test du doctrine:generate:crud pour la création/modification de vins et d'univers. Correction de quelques bugs

---
+++ src/ProductBundle/Form/WineType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace ProductBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use ProductBundle\Form\ProductType;
+
+class WineType extends ProductType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+        $builder->add('vintage')->add('color')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ProductBundle\Entity\Wine'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'productbundle_wine';
+    }
+
+
+}
