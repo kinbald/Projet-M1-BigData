6c532bf6847b06ff76f0603fc3b8e4dce9eb7e52
Camille <chiquetcamille@gmail.com>
2017-01-16 18:38:57 +0100

    Ajout fonction validation médias pour l'admin

--- src/UserBundle/DataFixtures/ORM/LoadUserData.php
+++ src/UserBundle/DataFixtures/ORM/LoadUserData.php
@@ -46,13 +46,41 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
         $userMedia->setUsernameCanonical('media');
         $userMedia->setEmail('mediaMedia@gmail.com');
         $userMedia->setEmailCanonical('mediaMedia@gmail.com');
-        $userMedia->setEnabled(true);
+        $userMedia->setEnabled(false);
         $userMedia->setPassword('$2y$12$VtTfiZZ68ThnssIEc8vkp.pkCL9YGAKB85Zk/8Up0MWg85t2h3drf');
         $userMedia->setFirstname('Journaliste');
         $userMedia->setLastname('chépakoimaitre');
         $userMedia->setCompanyName('Le Bon Pinnard');
         $userMedia->setIdPresse('168464358');
         $userMedia->setUrlBlog('LeBonPinnard.com');
+        $manager->persist($userMedia);
+
+        $userMedia = new UserMedia();
+        $userMedia->setUsername('media2');
+        $userMedia->setUsernameCanonical('media2');
+        $userMedia->setEmail('vziuregLvnzougvb@gmail.com');
+        $userMedia->setEmailCanonical('vziuregLvnzougvb@gmail.com');
+        $userMedia->setEnabled(false);
+        $userMedia->setPassword('$2y$12$VtTfiZZ68ThnssIEc8vkp.pkCL9YGAKB85Zk/8Ur3MWg85t2h3drf');
+        $userMedia->setFirstname('vsqfgljn');
+        $userMedia->setLastname('vlziehglz');
+        $userMedia->setCompanyName('Magazine sur du vin');
+        $userMedia->setIdPresse('168464360');
+        $userMedia->setUrlBlog('blablalbaalbalbla.com');
+        $manager->persist($userMedia);
+
+        $userMedia = new UserMedia();
+        $userMedia->setUsername('media3');
+        $userMedia->setUsernameCanonical('media3');
+        $userMedia->setEmail('zlvhleivjelivj@gmail.com');
+        $userMedia->setEmailCanonical('zlvhleivjelivj@gmail.com');
+        $userMedia->setEnabled(false);
+        $userMedia->setPassword('$2y$12$VtTfiZZ68ThnssIEc8vkp.pkCL9YGAKB85Zk/8Ur3Mjgk8t2h3drf');
+        $userMedia->setFirstname('vepojtgo');
+        $userMedia->setLastname('erfcvek');
+        $userMedia->setCompanyName('un autre magazine');
+        $userMedia->setIdPresse('134864785');
+        $userMedia->setUrlBlog('blablavzelijla.com');
         $manager->persist($userMedia);
 
         $manager->flush();
