944e7daed49a9332c241eef2ffaca5fedeee15a3
morpheam <olive.canal@gmail.com>
2017-01-27 12:12:57 +0100

    Correction de la navbar une fois connecté. Ajout du crud commande (purchase) Reste à faire : habillage css des pages générées (edit, show...)

---
+++ src/ProductBundle/Form/PurchaseType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ProductBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PurchaseType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('address')->add('city')->add('postalCode')->add('country')->add('done')->add('dateOrder')->add('user')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ProductBundle\Entity\Purchase'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'productbundle_purchase';
+    }
+
+
+}
