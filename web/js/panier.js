68a54801cffa5e5b2c832ea457d8b9d91102458c
onlythexavier <xavier.dupont@isen.yncrea.fr>
2017-01-27 10:05:16 +0100

    Page panier html avec photos + navbar import excel

---
+++ web/js/panier.js
@@ -0,0 +1,13 @@
+function ajoutProduit( nom ) {
+    document.getElementById( nom ).value ++;
+}
+function retireProduit( nom ) {
+    if ( document.getElementById( nom ).value > 0 )
+        document.getElementById( nom ).value --;
+}
+function isNumberKey(evt) {
+    var charCode = (evt.which) ? evt.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}
