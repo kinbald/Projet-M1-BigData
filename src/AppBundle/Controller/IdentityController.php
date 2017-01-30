73d9c388d84aa233ef9b75ed378b51bb5c08ba57
onlythepg <pgdu13@gmail.com>
2017-01-30 15:06:31 +0100

    Ajout de "Qui-sommes-nous" + modification en csq de "Contact"

---
+++ src/AppBundle/Controller/IdentityController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use ProductBundle\Entity\PictureUniverse;
+use ProductBundle\Entity\Universe;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class IdentityController extends Controller
+{
+    /**
+     * @Route("/qui-sommes-nous", name="qui-sommes-nous")
+     */
+    public function indexAction(Request $request)
+    {
+        $pictureArray = array();
+        $em = $this->getDoctrine()->getManager();
+        $universes = $em->getRepository('ProductBundle:Universe')->findAll();
+        foreach ($universes as $universe) {
+            $pictures=$universe->getPictures();
+            array_push($pictureArray, $pictures[0]);
+
+        }
+
+
+        return $this->render('AppBundle:Identity:identity.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..') . DIRECTORY_SEPARATOR,
+            'universes' => $universes,
+            'product_pictures' => $pictureArray,
+        ]);
+    }
+
+
+}
