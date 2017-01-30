8ad7e8309a49704582e3f6afffef2e33211e20bb
Jérémy Tablet <jeremy.tablet@gmail.com>
2017-01-30 10:56:06 +0100

    Corrections Données BDD & Affichage (Sitewide, partie 1)

--- src/ProductBundle/DataFixtures/ORM/LoadProducts.php
+++ src/ProductBundle/DataFixtures/ORM/LoadProducts.php
@@ -27,7 +27,7 @@ class LoadProducts extends AbstractFixture implements OrderedFixtureInterface
         $wine1->setVintage(new \DateTime('2007-01-01 10:00:00'));
         $wine1->setColor('Rouge');
         $wine1->setName('Vin Gigondas rouge');
-        $wine1->setDescription('c\'est du vin');
+        $wine1->setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
         $wine1->setVolume(1.5);
         $wine1->setPrice(135.40);
         $wine1->setStock(30);
@@ -43,7 +43,7 @@ class LoadProducts extends AbstractFixture implements OrderedFixtureInterface
         $wine2->setVintage(new \DateTime('2012-01-01 10:00:00'));
         $wine2->setColor('Rouge');
         $wine2->setName('Vin Montauriol rouge');
-        $wine2->setDescription('c\'est du vin');
+        $wine2->setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
         $wine2->setVolume(1.5);
         $wine2->setPrice(48.70);
         $wine2->setStock(20);
@@ -59,7 +59,7 @@ class LoadProducts extends AbstractFixture implements OrderedFixtureInterface
         $wine3->setVintage(new \DateTime('2015-01-01 10:00:00'));
         $wine3->setColor('Rose');
         $wine3->setName('Ocean Rose');
-        $wine3->setDescription('Une cuvée entre Dunes et Océan');
+        $wine3->setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
         $wine3->setVolume(0.7);
         $wine3->setPrice(12.70);
         $wine3->setStock(150);
@@ -74,8 +74,8 @@ class LoadProducts extends AbstractFixture implements OrderedFixtureInterface
         $wine4 = new Wine();
         $wine4->setVintage(new \DateTime('2014-01-01 10:00:00'));
         $wine4->setColor('Rose');
-        $wine4->setName('Chateau les crostes');
-        $wine4->setDescription('Côtes de Provence');
+        $wine4->setName('Chateau Toulon');
+        $wine4->setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
         $wine4->setVolume(0.3);
         $wine4->setPrice(9.56);
         $wine4->setStock(110);
@@ -92,7 +92,7 @@ class LoadProducts extends AbstractFixture implements OrderedFixtureInterface
         $spiritueux1 = new Spirit();
         $spiritueux1->setAlcoholDegree(50);
         $spiritueux1->setName('Absolute vodka');
-        $spiritueux1->setDescription('Absolute vodka 100 Country of Sweden');
+        $spiritueux1->setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
         $spiritueux1->setVolume(1);
         $spiritueux1->setPrice(56.20);
         $spiritueux1->setStock(60);
@@ -107,7 +107,7 @@ class LoadProducts extends AbstractFixture implements OrderedFixtureInterface
         $spiritueux2 = new Spirit();
         $spiritueux2->setAlcoholDegree(40);
         $spiritueux2->setName('Canadian Whisky');
-        $spiritueux2->setDescription('Canadian Club Classic, aged 12 years whisky');
+        $spiritueux2->setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
         $spiritueux2->setVolume(0.7);
         $spiritueux2->setPrice(43.15);
         $spiritueux2->setStock(35);
@@ -122,7 +122,7 @@ class LoadProducts extends AbstractFixture implements OrderedFixtureInterface
         $spiritueux3 = new Spirit();
         $spiritueux3->setAlcoholDegree(40);
         $spiritueux3->setName('Belvedere vodka');
-        $spiritueux3->setDescription('Belvedere vodka, unfiltred');
+        $spiritueux3->setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
         $spiritueux3->setVolume(0.7);
         $spiritueux3->setPrice(35.45);
         $spiritueux3->setStock(59);
@@ -137,7 +137,7 @@ class LoadProducts extends AbstractFixture implements OrderedFixtureInterface
         $spiritueux4 = new Spirit();
         $spiritueux4->setAlcoholDegree(40);
         $spiritueux4->setName('Whisky Red Label');
-        $spiritueux4->setDescription('Old scotch Whisky Red Label Johnnie Walker');
+        $spiritueux4->setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
         $spiritueux4->setVolume(0.7);
         $spiritueux4->setPrice(53.05);
         $spiritueux4->setStock(56);
@@ -152,7 +152,7 @@ class LoadProducts extends AbstractFixture implements OrderedFixtureInterface
 
         $purchase5 = new Purchase();
         $purchase5->setUser($user);
-        $purchase5->setAddress('trololololo');
+        $purchase5->setAddress('Avenue du Port');
         $purchase5->setCity('Hyères');
         $purchase5->setPostalCode('83400');
         $purchase5->setCountry('FR');
@@ -170,68 +170,68 @@ class LoadProducts extends AbstractFixture implements OrderedFixtureInterface
         $productEvaluation->setProduct($wine1);
         $productEvaluation->setUser($userConsumer);
         $productEvaluation->setMark(15);
-        $productEvaluation->setReview('cété trai bon lol');
+        $productEvaluation->setReview('Vin de très bonne qualité, je le recommande');
         $manager->persist($productEvaluation);
 
 
         /*-----------------------LES UNIVERS-------------------------*/
         /*-----------------------------------------------------------*/
         $universe = new Universe();
-        $universe->setName('Vins de femmes');
-        $universe->setDescription('Vin pour femmes');
+        $universe->setName('Femmes et vin du monde');
+        $universe->setDescription('Sélection de femmes expertes internationales');
         $universe->addProduct($wine1);
         $universe->addProduct($wine2);
         $universe->addProduct($wine3);
         $manager->persist($universe);
 
         $picture = new PictureUniverse();
-        $picture->setAlt('vin');
+        $picture->setAlt('Femmes et vin du monde');
         $picture->setUrl('univ_femme.jpg');
         $picture->setUniverse($universe);
         $manager->persist($picture);
 
         /*-----------------------------------------------------------*/
         $universe = new Universe();
-        $universe->setName('Vins gastronomiques');
-        $universe->setDescription('Vins pour associaition à des mets');
+        $universe->setName('Femmes et vin de France');
+        $universe->setDescription('Sélection de femmes expertes internationales');
         $universe->addProduct($wine4);
         $manager->persist($universe);
 
         $picture = new PictureUniverse();
-        $picture->setAlt('vin');
-        $picture->setUrl('univ_gastronomie.jpg');
+        $picture->setAlt('Femmes et vin de France');
+        $picture->setUrl('univ_france.jpg');
         $picture->setUniverse($universe);
         $manager->persist($picture);
 
         /*-----------------------------------------------------------*/
         $universe = new Universe();
-        $universe->setName('Vins du monde');
-        $universe->setDescription('Vins orginaux de tout pays');
+        $universe->setName('Femmes et spiritueux du monde');
+        $universe->setDescription('Sélection de femmes expertes internationales');
         $universe->addProduct($wine1);
         $manager->persist($universe);
 
         $picture = new PictureUniverse();
-        $picture->setAlt('vin');
+        $picture->setAlt('Femmes et spiritueux du monde');
         $picture->setUrl('univ_monde.jpg');
         $picture->setUniverse($universe);
         $manager->persist($picture);
 
         /*-----------------------------------------------------------*/
         $universe = new Universe();
-        $universe->setName('Vins rosés');
-        $universe->setDescription('Vins rosés');
+        $universe->setName('Vins de terroir');
+        $universe->setDescription('Sélection d\'experts internationaux');
         $manager->persist($universe);
 
         $picture = new PictureUniverse();
-        $picture->setAlt('vin');
-        $picture->setUrl('univ_rose.jpg');
+        $picture->setAlt('terroir');
+        $picture->setUrl('univ_terroir.jpg');
         $picture->setUniverse($universe);
         $manager->persist($picture);
 
          /*-----------------------------------------------------------*/
          $universe = new Universe();
-         $universe->setName('Spiritueux');
-         $universe->setDescription('Spiritueux');
+        $universe->setName('Vin de gastronomie');
+        $universe->setDescription('Sélection d\'experts internationaux');
 
         $universe->addProduct($spiritueux1);
         $universe->addProduct($spiritueux2);
@@ -241,21 +241,28 @@ class LoadProducts extends AbstractFixture implements OrderedFixtureInterface
 
 
          $picture = new PictureUniverse();
-         $picture->setAlt('spiritueux');
-         $picture->setUrl('univ_spiritueux.jpg');
+        $picture->setAlt('gastronomie');
+        $picture->setUrl('univ_gastronomie.jpg');
          $picture->setUniverse($universe);
          $manager->persist($picture);
 
+        /*-----------------------------------------------------------*/
+        $universe = new Universe();
+        $universe->setName('Vin rosés du monde');
+        $universe->setDescription('Sélection d\'experts internationaux');
+
+        $universe->addProduct($spiritueux1);
+        $universe->addProduct($spiritueux2);
+        $universe->addProduct($spiritueux3);
+        $universe->addProduct($spiritueux4);
+        $manager->persist($universe);
 
 
-
-
-
-
-
-
-
-
+        $picture = new PictureUniverse();
+        $picture->setAlt('vin rosés');
+        $picture->setUrl('univ_rose.jpg');
+        $picture->setUniverse($universe);
+        $manager->persist($picture);
 
         $manager->flush();
     }
