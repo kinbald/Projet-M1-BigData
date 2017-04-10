1d2931aadc3fd1a21ef0dfe1845e9f75506b02f7
Camille <chiquetcamille@gmail.com>
2017-04-10 11:50:42 +0200

    [PROJM117-174] MAJ Panier

---
+++ src/ProductBundle/Form/ConditioningSelectType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ProductBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use ProductBundle\Form\ProductType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RangeType;
+class ConditioningSelectType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('conditioning', ChoiceType::class, array('choices' => $options['data']['values'], 'required' => true, 'label' => false, 'attr' => array('maxlength' => 255,
+            'class' => 'champ conditioning rounded')));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'productbundle_conditioningselect';
+    }
+
+}
