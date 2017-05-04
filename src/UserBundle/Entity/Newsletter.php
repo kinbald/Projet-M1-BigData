f0cea449781f7d53dd13e77666c503e5124c5fd0
Julien Cassagne <julien.e.cassagne@gmail.com>
2017-05-04 09:43:26 +0200

    [PROJM117-271] Ajout de la table Newsletter

---
+++ src/UserBundle/Entity/Newsletter.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Newsletter
+ *
+ * @ORM\Table(name="newsletter")
+ * @ORM\Entity(repositoryClass="UserBundle\Repository\NewsletterRepository")
+ */
+class Newsletter
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mail", type="string", length=255, unique=true)
+     */
+    private $mail;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set mail
+     *
+     * @param string $mail
+     *
+     * @return Newsletter
+     */
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    /**
+     * Get mail
+     *
+     * @return string
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+}
+
